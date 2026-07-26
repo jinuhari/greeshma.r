10d5cae755029cb7d841962cd7bc91b75ee986a8
Greeshma <greeshma6045@gmail.com>
2026-07-26 17:49:32 +0530

    Add inline SVG favicon (letter G)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -82,6 +82,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>G</text></svg>" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
